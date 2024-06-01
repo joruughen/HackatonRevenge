89fd2b6042d00075d6a351a849a8963027133edd
THINKPAD <jorge.quenta@utec.edu.pe>
2024-06-01 15:56:20 -0500

    aaaa

---
+++ src/main/java/Paquete/ListadeReproduccion/ListadeReproduccion.java
@@ -0,0 +1,30 @@
+package Paquete.ListadeReproduccion;
+
+import Paquete.Entidad1.Dominio.Cancion;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+public class ListadeReproduccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idPlaylist;
+
+    private String nombre;
+
+    private Integer idUser;
+
+    private Date fechaDeCreacion;
+
+    @JsonManagedReference
+    @ManyToMany
+    @JoinTable(name = "ListadeReproduccion_Cancion",
+            joinColumns = @JoinColumn(name = "ListadeReproduccion_id"),
+            inverseJoinColumns = @JoinColumn(name = "Cancion_id"))
+    private List<Cancion> canciones;
+}
---
+++ src/main/java/Paquete/ListadeReproduccion/ListadeReproduccionRepositorio.java
@@ -0,0 +1,8 @@
+package Paquete.ListadeReproduccion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ListadeReproduccionRepositorio extends JpaRepository<ListadeReproduccion, Integer> {
+    List<ListadeReproduccion> findAllByUserId(Integer userId);
+}
